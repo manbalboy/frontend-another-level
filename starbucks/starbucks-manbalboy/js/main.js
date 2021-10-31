8a2f965fcbad573106108bae03119f7f58f91818
manbalboy <manbalboy@hanmail.net>
2021-10-31 23:43:23 +0900

    뱃지 gsap

--- starbucks/starbucks-manbalboy/js/main.js
+++ starbucks/starbucks-manbalboy/js/main.js
@@ -7,10 +7,30 @@ searchEl.addEventListener('click', (e) => {
 
 searchInputEl.addEventListener('focus', (e) => {
   searchEl.classList.add('focused');
-  searchInputEl.setAttribute("placeholder","통합 검색");
-})
+  searchInputEl.setAttribute("placeholder", "통합 검색");
+});
 
 searchInputEl.addEventListener('blur', (e) => {
   searchEl.classList.remove('focused');
-  searchInputEl.setAttribute("placeholder","");
-})
+  searchInputEl.setAttribute("placeholder", "");
+});
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+  if (window.scrollY > 500) {
+    //배지 숨기기
+    // badgeEl.style.display = 'none';
+    gsap.to(badgeEl, 0.6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    //배지 보이기
+    // badgeEl.style.display = 'block';
+    gsap.to(badgeEl, 0.6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
