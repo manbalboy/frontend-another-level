ed80131a7fac48e924c1af96e0bf47c035b5e46e
manbalboy <manbalboy@hanmail.net>
2021-11-11 22:53:24 +0900

    optional property declare

---
+++ typescript/ch05/interface3.ts
@@ -0,0 +1,15 @@
+interface Person3 {
+    name: string;
+    age?: number;
+    [index: string]: any; /*인덱서블*/
+}
+
+
+function hello3(person: Person3): void {
+    console.log(`안녕하세요 ${person.name} 입니다.`);
+}
+
+const p1: Person3 = {
+    name: '정훈',
+    test: 'test'
+}
