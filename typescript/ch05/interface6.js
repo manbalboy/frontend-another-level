0b227d11837ff34b1b62e86ce51a7f2881a2eaa5
manbalboy <manbalboy@hanmail.net>
2021-11-12 22:25:19 +0900

    readonly interface declare

---
+++ typescript/ch05/interface6.js
@@ -0,0 +1,5 @@
+"use strict";
+var k = {
+    name: "이웅재",
+    city: "서울"
+};
---
+++ typescript/ch05/interface7.js
@@ -0,0 +1,4 @@
+"use strict";
+var helloPerson = function (name, age) {
+    console.log("\uC548\uB155\uD558\uC138\uC694! " + name + " \uC785\uB2C8\uB2E4.");
+};
