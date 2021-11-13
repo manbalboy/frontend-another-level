9e14c71671af502bfbada2892d5dab49a419f658
manbalboy <manbalboy@hanmail.net>
2021-11-13 16:39:43 +0900

    [jest/react] 카운터 숫자 구현하기

--- jest/react/react-test-app/src/App.test.js
+++ jest/react/react-test-app/src/App.test.js
@@ -1,10 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("the counter starts at 0", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // screen object를 이용해서 원하는 엘레멘트에 접근 (접근할 떄 ID로)
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(0);
 
   // const lintTest = screen.getByRole('button', {
   //   name: 'lintTest'
