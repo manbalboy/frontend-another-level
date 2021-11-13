c22472edf7db28bce6fe49d55e30548eb270c3ea
manbalboy <manbalboy@hanmail.net>
2021-11-13 09:05:07 +0900

    typescript readme file add or class study 1

---
+++ typescript/ch06/example.js
@@ -0,0 +1,9 @@
+"use strict";
+var Person = /** @class */ (function () {
+    function Person(name) {
+        this.name = name;
+    }
+    return Person;
+}());
+var p1 = new Person("Hun");
+console.log(p1);
---
+++ typescript/ch06/example.ts
@@ -0,0 +1,10 @@
+class Person {
+    name: string;
+    constructor(name: string) {
+        this.name = name;
+    }
+}
+
+const p1 = new Person("Hun");
+
+console.log(p1);
