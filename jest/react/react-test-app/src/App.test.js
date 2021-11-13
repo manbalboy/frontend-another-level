8ea1707fb117bb4cca2f9642136428b2579b59f7
manbalboy <manbalboy@hanmail.net>
2021-11-13 17:04:08 +0900

    [jest/react] +, - 버튼 텍스트 test 작성

--- jest/react/react-test-app/src/App.test.js
+++ jest/react/react-test-app/src/App.test.js
@@ -3,13 +3,21 @@ import App from "./App";
 
 test("the counter starts at 0", () => {
   render(<App />);
-  // screen object를 이용해서 원하는 엘레멘트에 접근 (접근할 떄 ID로)
+  // screen object 를 이용해서 원하는 엘레멘트에 접근 (접근할 떄 ID로)
   const counterElement = screen.getByTestId("counter");
   expect(counterElement).toHaveTextContent(0);
+});
 
-  // const lintTest = screen.getByRole('button', {
-  //   name: 'lintTest'
-  // });
-  //
-  // expect(lintTest).toHaveTextContent('lintTest');
+test("minus button has correct text", () => {
+  render(<App />);
+  // screen object 를 이용해서 원하는 엘레멘트에 접근 (접근할 떄 ID로)
+  const minusButtonElement = screen.getByTestId("minus-button");
+  expect(minusButtonElement).toHaveTextContent("-");
+});
+
+test("plus button has correct text", () => {
+  render(<App />);
+  // screen object 를 이용해서 원하는 엘레멘트에 접근 (접근할 떄 ID로)
+  const plusButtonElement = screen.getByTestId("plus-button");
+  expect(plusButtonElement).toHaveTextContent("+");
 });
