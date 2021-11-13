97f033d99ea05552141bb95125baf5ebedd010b8
manbalboy <manbalboy@hanmail.net>
2021-11-13 12:43:12 +0900

    index signatures in class

---
+++ typescript/ch06/example6.ts
@@ -0,0 +1,17 @@
+//class => object
+// {mark:'male', jade: 'male'}
+// {chloe:'female', alex: 'male', anna: 'female'}
+class Students {
+    [index: string]: "male" | "female" ;
+}
+
+
+const a = new Students();
+a.mark = 'male';
+a.jade = 'male';
+
+console.log(a);
+
+const b = new Students();
+b.chloe = 'female';
+b.alex = 'female';
