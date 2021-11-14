4956c2f27f13b40bbcb50c4cac79430e85f6106d
manbalboy <manbalboy@hanmail.net>
2021-11-14 13:19:42 +0900

    [jest/shop] 상품 가격 test 작성

---
+++ jest/react-shop-test/react-shop/src/pages/OrderPage/tests/calculate.test.js
@@ -0,0 +1,21 @@
+import Type from "../Type";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+
+test("update product's total when products change", async () => {
+  render(<Type orderType="products" />);
+
+  const productsTotal = screen.getByText("삼품 총 가격:", { exact: false });
+
+  expect(productsTotal).toHaveTextContent("0");
+
+  //아메리카 여행 상품 한개 올리기
+  const americaInput = await screen.findByRole("spinbutton", {
+    name: "America",
+  });
+
+  userEvent.clear(americaInput);
+  userEvent.type(americaInput, "1");
+
+  expect(productsTotal).toHaveTextContent("1000");
+});
