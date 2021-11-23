6bfe574dcedb3c221418cc25fba4a8410637ee9e
manbalboy <manbalboy@hanmail.net>
2021-11-24 00:00:30 +0900

    vue router setting

--- vue/vue-start/vue3-movie-app/.eslintrc.js
+++ vue/vue-start/vue3-movie-app/.eslintrc.js
@@ -1,34 +1,43 @@
 module.exports = {
-  // 사전에 정의된 전역 변수 설정
+  root: true,
   env: {
-    browser: true,
-    node: true
+    node: true,
   },
-  // 사용 가능한 규칙 세트
-  extends: [
-    // 'plugin:vue/vue3-essential', // Lv1
-    'plugin:vue/vue3-strongly-recommended', // Lv2
-    // 'plugin:vue/vue3-recommended', // Lv3
-    'eslint:recommended'
-  ],
-  // 구문 분석할 패키지 설정(Babel, ES6^ => ES5)
+  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: "babel-eslint",
   },
-  // 추가적인 코드 규칙을 설정
   rules: {
-    'vue/html-self-closing': ['error', {
-      html: {
-        void: 'always',
-        normal: 'never',
-        component: 'always'
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid', //가능하면 생략 , always 항상 써야한다.
+        proseWrap: 'preserve',
+        jsxBracketSameLine: false,
+        htmlWhitespaceSensitivity: 'strict',
+        vueIndentScriptAndStyle: true, // script 영역의 들여쓰기  true false
+        endOfLine: 'auto',
       },
-      svg: 'always',
-      math: 'always'
-    }],
-    'vue/html-closing-bracket-newline': ['error', {
-      singleline: 'never',
-      multiline: 'never'
-    }]
-  }
-}
+    ],
+  },
+  overrides: [
+    {
+      files: [
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
