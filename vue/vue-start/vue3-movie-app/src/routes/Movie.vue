a8aaa182f9ffccde5b070a3176c6ac00f1b84e15
manbalboy <manbalboy@hanmail.net>
2021-12-04 01:15:49 +0900

    영화 검색 상세화면 마크업

--- vue/vue-start/vue3-movie-app/src/routes/Movie.vue
+++ vue/vue-start/vue3-movie-app/src/routes/Movie.vue
@@ -1,17 +1,59 @@
 <template>
   <div class="container">
-    <div class="skeletons">
-      <div class="skeleton poster"></div>
+    <template v-if="loading">
+      <div class="skeletons">
+        <div class="skeleton poster"></div>
+        <div class="specs">
+          <div class="skeleton title"></div>
+          <div class="skeleton spec"></div>
+          <div class="skeleton plot"></div>
+          <div class="skeleton etc"></div>
+          <div class="skeleton etc"></div>
+          <div class="skeleton etc"></div>
+        </div>
+      </div>
+      <Loader :size="3" :z-index="9" fixed />
+    </template>
+
+    <div v-else class="movie-details">
+      <div
+        class="poster"
+        :style="{ backgroundImage: `url(${theMovie.Poster})` }"
+      >
+      </div>
       <div class="specs">
-        <div class="skeleton title"></div>
-        <div class="skeleton spec"></div>
-        <div class="skeleton plot"></div>
-        <div class="skeleton etc"></div>
-        <div class="skeleton etc"></div>
-        <div class="skeleton etc"></div>
+        <div class="title">
+          {{ theMovie.Title }}
+        </div>
+        <div class="labels">
+          <span>{{ theMovie.Released }}</span>
+          <span>{{ theMovie.Runtime }}</span>
+          <span>{{ theMovie.Country }}</span>
+        </div>
+        <div class="plot">
+          {{ theMovie.Plot }}
+        </div>
+        <div class="ratings">
+          <h3>Ratings</h3>
+        </div>
+        <div>
+          <h3>Actors</h3>
+          {{ theMovie.Actors }}
+        </div>
+        <div>
+          <h3>Director</h3>
+          {{ theMovie.Director }}
+        </div>
+        <div>
+          <h3>Production</h3>
+          {{ theMovie.Production }}
+        </div>
+        <div>
+          <h3>Genre</h3>
+          {{ theMovie.Genre }}
+        </div>
       </div>
     </div>
-    <Loader :size="3" :z-index="9" fixed />
   </div>
 </template>
 
@@ -20,6 +62,15 @@
   export default {
     name: 'Movie',
     components: { Loader },
+    computed: {
+      theMovie() {
+        return this.$store.state.movie.theMovie;
+      },
+
+      loading() {
+        return this.$store.state.movie.loading;
+      },
+    },
     created() {
       this.$store.dispatch('movie/searchMovieWithId', {
         id: this.$route.params.id,
@@ -67,6 +118,78 @@
         width: 50%;
         height: 50px;
         margin-top: 20px;
+      }
+    }
+  }
+  .movie-details {
+    display: flex;
+    color: $gray-600;
+
+    .poster {
+      width: 500px;
+      height: 500px * 3/2;
+      margin-right: 70px;
+      border-radius: 10px;
+      background-color: $gray-200;
+      background-size: cover;
+      background-position: center;
+      position: relative;
+      flex-shrink: 0;
+    }
+
+    .specs {
+      flex-grow: 1;
+
+      .title {
+        color: $black;
+        font-family: 'Oswald', sans-serif;
+        font-size: 70px;
+        line-height: 1;
+        margin-bottom: 30px;
+      }
+
+      .labels {
+        color: $primary;
+
+        span {
+          &::after {
+            content: '\00b7';
+            margin: 0 6px;
+          }
+
+          &:last-child::after {
+            display: none;
+          }
+        }
+      }
+
+      .plot {
+        margin-top: 20px;
+      }
+
+      .ratings {
+        .rating-wrap {
+          display: flex;
+
+          .rating {
+            display: flex;
+            align-items: center;
+            margin-right: 32px;
+
+            img {
+              height: 30px;
+              flex-shrink: 0;
+              margin-right: 6px;
+            }
+          }
+        }
+      }
+
+      h3 {
+        margin: 24px 0 6px;
+        color: $black;
+        font-family: 'Oswald', sans-serif;
+        font-size: 20px;
       }
     }
   }
