0fc35afb72285155db2e9cb34c8613b613463600
manbalboy <manbalboy@hanmail.net>
2021-12-04 13:11:43 +0900

    nav 적용

--- vue/vue-start/vue3-movie-app/src/components/Header.vue
+++ vue/vue-start/vue3-movie-app/src/components/Header.vue
@@ -3,7 +3,12 @@
     <Logo></Logo>
     <div class="nav nav-pills">
       <div v-for="nav in navigations" :key="nav.name" class="nav-item">
-        <RouterLink class="nav-link" :to="nav.href" active-class="active">
+        <RouterLink
+          class="nav-link"
+          :to="nav.href"
+          active-class="active"
+          :class="{ active: isMatch(nav.path) }"
+        >
           {{ nav.name }}
         </RouterLink>
       </div>
@@ -25,6 +30,7 @@
           {
             name: 'Movie',
             href: '/movie/tt4520988',
+            path: /^\/movie/,
           },
           {
             name: 'About',
@@ -35,6 +41,13 @@
     },
     components: {
       Logo,
+    },
+
+    methods: {
+      isMatch(path) {
+        if (!path) return false;
+        return path.test(this.$route.fullPath);
+      },
     },
   };
 </script>
