ac7f50625fa487d494952454080a0ea5aed0efaf
manbalboy <manbalboy@hanmail.net>
2021-12-04 14:00:53 +0900

    about 작성

---
+++ vue/vue-start/vue3-movie-app/src/store/about.js
@@ -0,0 +1,13 @@
+export default {
+  namespace: true,
+  state: () => {
+    return {
+      name: 'manbalboy',
+      email: 'manbalboy@hanmail.net',
+      blog: 'https://manbalboy.github.io/',
+      phone: '+82-10-4856-0217',
+      image:
+        'https://github.com/manbalboy/profile/blob/master/images/JUNG2015.jpg?raw=true',
+    };
+  },
+};
