f77bc56617b9fd225c15c2de46465fd58514ab51
manbalboy <manbalboy@hanmail.net>
2021-12-19 22:21:36 +0900

    Webpack assets

--- webpack/study/webpack.config.js
+++ webpack/study/webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
 
 module.exports = {
+  mode : 'production',
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist_'),
+  },
+  module: {
+    rules: [
+      // css
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      // 이미지
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|avif)$/i,
+        type: 'asset/resource',
+      },
+      // 글꼴
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
